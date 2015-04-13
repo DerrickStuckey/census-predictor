fb9c1ac9e594a1621ec3de81e55534fed38c0877
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-13 11:07:38 -0400

    holdout sample partition code

---
+++ code/analysis/Data_Partition/zip_code_partition.R
@@ -0,0 +1,18 @@
+## Train/Test split for full practicum project
+## Testing data will be held for later use
+
+# read in population data
+pop <- read.csv("../../../prepared_data/population_by_zip.csv")
+
+#View(pop)
+data_size <- nrow(pop)
+train_size <- data_size*0.8
+
+set.seed(11235)
+trainindex <- sample(data_size, train_size, replace=FALSE)
+training <- pop[trainindex,]
+holdout <- pop[-trainindex,]
+
+write.csv(training, "../../../prepared_data/population_by_zip_train.csv", row.names=FALSE)
+write.csv(holdout, "../../../prepared_data/population_by_zip_holdout.csv", row.names=FALSE)
+
