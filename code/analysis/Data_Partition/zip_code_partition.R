995b73a40a615fdeb70589cae379df09ea4dc424
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-13 12:31:58 -0400

    pads zip code values w/ zeros before writing csv

--- code/analysis/Data_Partition/zip_code_partition.R
+++ code/analysis/Data_Partition/zip_code_partition.R
@@ -4,6 +4,10 @@
 # read in population data
 pop <- read.csv("../../../prepared_data/population_by_zip.csv")
 
+# convert zip code values to strings and pad with zeros
+library(stringr)
+pop$zipCode <- str_pad(as.character(pop$zipCode), 5, pad="0")
+
 #View(pop)
 data_size <- nrow(pop)
 train_size <- data_size*0.8
