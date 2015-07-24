9b8368fac2224fb60fb9e3fc20c8140b7aa708fd
DerrickStuckey <derrickstuckey@gmail.com>
2015-07-24 13:57:50 -0400

    consolidated duplicate zip codes crossing state lines

---
+++ R_Dataprep/remove_duplicates.R
@@ -0,0 +1,73 @@
+## Consolidate duplicate zip codes (which cross state lines) into single zip codes
+
+# load main data set
+PracticumData <- read.csv("../prepared_data/PracticumDataFull.csv", stringsAsFactors=FALSE)
+
+# extract columns representing counts
+CountsOnlyData <- subset(PracticumData, 
+                         select=c(X2010pop, X2010.SSrecip, IRS_rtrns, Beds, gasStations, 
+                                  count_fastfood, towers, careCenters, homeDaycare,
+                                  f_markets, walmart_stores, target_stores, CVSstores, 
+                                  home_depot_count, lowes_count, whole_foods_count,
+                                  basspro_count, starbucks_count, zipCode, stateCode))
+
+# load raw race count data
+pop_race_zip_derrick <- read.csv("../raw_data/pop_race_zip_derrick.csv", stringsAsFactors=FALSE)
+pop_race_zip_slim <- subset(pop_race_zip_derrick, select=-c(Population,Total_Race_Count))
+
+# merge race count data
+CountsWithRace <- merge(CountsOnlyData, pop_race_zip_slim, 
+                        by.x=c("zipCode","stateCode"),
+                        by.y=c("zip.code.tabulation.area","state"))
+
+# load raw age data
+pop_age_gender_zip <- read.csv("../raw_data/pop_age_gender_zip.csv", stringsAsFactors=FALSE)
+pop_age_gender_zip_slim <- subset(pop_age_gender_zip, select=-c(Population,Total_Population))
+
+# merge age, gender data
+FullCountsData <- merge(CountsWithRace, pop_age_gender_zip_slim, 
+                        by.x=c("zipCode","stateCode"),
+                        by.y=c("zip.code.tabulation.area","state"))
+
+# compute counts for other percentage variables, weighted sum by population
+FullCountsData$countbachelors <- PracticumData$percbachelors * PracticumData$X2010pop
+FullCountsData$countgraddegree <- PracticumData$percgraddegree * PracticumData$X2010pop
+
+# aggregate by zip code
+CountsDataAgged <- aggregate(FullCountsData, by=list(PracticumData$zipCode), FUN=sum)
+
+### Compute primary state for each zip code ###
+
+# sort by population descending
+SortedData <- PracticumData[order(PracticumData$X2010pop, decreasing=TRUE),]
+
+# find duplicate zip codes (index of 2nd entry for each duplicate)
+dupindices <- duplicated(SortedData$zipCode)
+
+DedupedData <- SortedData[!dupindices,]
+
+primaryZipMap <- data.frame("zipCode"=DedupedData$zipCode,
+                            "stateCode"=DedupedData$stateCode,
+                            "state"=DedupedData$state)
+
+write.csv(primaryZipMap, file="../prepared_data/zip_with_primary_state.csv",
+          row.names=FALSE, quote=FALSE)
+
+### For variables not corresponding to population counts, ###
+### take the value from the zip/state
+### entry corresponding to the primary state for that zip code 
+
+PrimaryStateData <- subset(DedupedData, select=c(zipCode,medhhsincome,homeprice_201501,
+                                                 rent_201501,valuechange_5year,
+                                                 com_elecrate, ind_elecrate, res_elecrate))
+PrimaryStateData$primaryState <- DedupedData$state
+PrimaryStateData$primaryStateCode <- DedupedData$stateCode
+
+FinishedData <- merge(CountsDataAgged, PrimaryStateData, by="zipCode")
+
+#remove a few erroneous columns
+FinishedData <- subset(FinishedData, select=-c(stateCode,Group.1))
+
+#write the finished data
+write.csv(FinishedData, file="../prepared_data/PracticumDataDedupedZips.csv",
+          row.names=FALSE, quote=FALSE)
