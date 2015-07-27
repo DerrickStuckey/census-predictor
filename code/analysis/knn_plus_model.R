5279d31fb80239c38e77bbf6ec905ce663514b50
DerrickStuckey <derrickstuckey@gmail.com>
2015-07-27 13:25:38 -0400

    added retail, zillow models - neither very good

--- code/analysis/knn_plus_model.R
+++ code/analysis/knn_plus_model.R
@@ -62,15 +62,84 @@ summary(knn_asian_loc$pred)
 rsq_val(knn_asian_loc$pred,validation_data_withlat$percasian)
 
 ## obtain knn predictions using other predictors
+# retail_only_training <- subset(training_data, 
+#                                select=c(starbucks_count, count_fastfood, whole_foods_count,
+#                                         walmart_stores, target_stores, home_depot_count,
+#                                         lowes_count, basspro_count, f_markets))
+# retail_only_validation <- subset(validation_data, 
+#                                select=c(starbucks_count, count_fastfood, whole_foods_count,
+#                                         walmart_stores, target_stores, home_depot_count,
+#                                         lowes_count, basspro_count, f_markets))
 retail_only_training <- subset(training_data, 
                                select=c(starbucks_count, count_fastfood, whole_foods_count,
                                         walmart_stores, target_stores, home_depot_count,
-                                        lowes_count, basspro_count, f_markets))
+                                        lowes_count, basspro_count, f_markets, CVSstores))
 
-knn_
+retail_only_validation <- subset(validation_data, 
+                               select=c(starbucks_count, count_fastfood, whole_foods_count,
+                                        walmart_stores, target_stores, home_depot_count,
+                                        lowes_count, basspro_count, f_markets, CVSstores))
 
-  
-#   Zip3Data$starbucks_count + Zip3Data$count_fastfood + 
-#   Zip3Data$whole_foods_count + Zip3Data$walmart_stores + Zip3Data$target_stores
+## attempt at per-capita retail counts
+# retail_percapita_training <- retail_only_training * 1000 / (training_data$IRS_rtrns)
+# retail_only_training <- retail_percapita_training
+# retail_percapita_validation <- retail_only_validation * 1000 / (validation_data$IRS_rtrns)
+# retail_only_validation <- retail_percapita_validation
+# set all NA values to 0
+# retail_only_training[is.na(retail_only_training)] <- 0
+# retail_only_validation[is.na(retail_only_validation)] <- 0
+## per-capita results no better
+
+# obtain retail knn validation predictions for percent white
+knn_white_retail <- knn.reg(train=retail_only_training,test=retail_only_validation,
+                         y=training_data$percwhite,k=4)
+summary(knn_white_retail$pred)
+rsq_val(knn_white_retail$pred,validation_data$percwhite)
+
+# obtain retail knn validation predictions for percent black
+knn_black_retail <- knn.reg(train=retail_only_training,test=retail_only_validation,
+                            y=training_data$percblack,k=4)
+summary(knn_black_retail$pred)
+rsq_val(knn_black_retail$pred,validation_data$percblack)
+
+# obtain retail knn validation predictions for percent asian
+knn_asian_retail <- knn.reg(train=retail_only_training,test=retail_only_validation,
+                            y=training_data$percasian,k=4)
+summary(knn_asian_retail$pred)
+rsq_val(knn_asian_retail$pred,validation_data$percasian)
+
+# Aggh: negative R-sq values for each retail data model!
+
+## zillow + lat/lon data knn model
+
+# use only training data w/o NA for zillow data
+zillow_nna_training <- training_data[!is.na(training_data$homeprice_201501) &
+                                               !is.na(training_data$rent_201501),]
+zillow_only_training <- subset(zillow_nna_training, 
+                               select=c(homeprice_201501,rent_201501))
+
+# same for validation data
+zillow_nna_validation <- validation_data[!is.na(validation_data$homeprice_201501) &
+                                                   !is.na(validation_data$rent_201501),]
+zillow_only_validation <- subset(zillow_nna_validation, 
+                               select=c(homeprice_201501,rent_201501))
+
+# zillow percentage white model
+knn_white_zillow <- knn.reg(train=zillow_only_training,test=zillow_only_validation,
+                            y=zillow_nna_training$percwhite,k=4)
+summary(knn_white_zillow$pred)
+rsq_val(knn_white_zillow$pred,zillow_nna_validation$percwhite)
+
+# zillow percentage black model
+knn_black_zillow <- knn.reg(train=zillow_only_training,test=zillow_only_validation,
+                            y=zillow_nna_training$percblack,k=4)
+summary(knn_black_zillow$pred)
+rsq_val(knn_black_zillow$pred,zillow_nna_validation$percblack)
+
+# zillow percentage asian model
+knn_asian_zillow <- knn.reg(train=zillow_only_training,test=zillow_only_validation,
+                            y=zillow_nna_training$percasian,k=4)
+summary(knn_asian_zillow$pred)
+rsq_val(knn_asian_zillow$pred,zillow_nna_validation$percasian)
 
 
