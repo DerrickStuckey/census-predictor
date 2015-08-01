61aa1f4a980c6e2f8a87653d0c85a79145458c33
DerrickStuckey <derrickstuckey@gmail.com>
2015-08-01 14:49:55 -0400

    saved formula to avoid re-running slow stepwise regression

--- code/analysis/pop_analysis_stepwise.R
+++ code/analysis/pop_analysis_stepwise.R
@@ -1,12 +1,12 @@
 ## exploratory analysis
 
-source("../code/utils/census_utils.R")
+source("../utils/census_utils.R")
 
 # load predictors
-Practicum_Predictors_Normalized <- read.csv("../prepared_data/Practicum_Predictors_Normalized.csv", stringsAsFactors=FALSE)
+Practicum_Predictors_Normalized <- read.csv("../../prepared_data/Practicum_Predictors_Normalized.csv", stringsAsFactors=FALSE)
 
 # load targets
-Practicum_Targets <- read.csv("../prepared_data/Practicum_Targets.csv", stringsAsFactors=FALSE)
+Practicum_Targets <- read.csv("../../prepared_data/Practicum_Targets.csv", stringsAsFactors=FALSE)
 
 # carve out holdout data
 Model_Target_Data <- Practicum_Targets[Practicum_Targets$partition==1,]
@@ -20,17 +20,33 @@ Pop_Model_Target_Data <- subset(Model_Target_Data, select=c(zipCode,pop_2010))
 Pop_Model_Data <- merge(Pop_Model_Target_Data,Practicum_Predictors_Normalized,by="zipCode",
                         all.x=TRUE,all.y=FALSE)
 
-# try linear regression to predict population
-library(MASS)
-poplmfit <- lm(pop_2010 ~ .-zipCode, data=Pop_Model_Data)
-popstep <- stepAIC(poplmfit, direction="both")
-summary(popstep)
+# stepwise linear regression
+# library(MASS)
+# poplmfit <- lm(pop_2010 ~ .-zipCode, data=Pop_Model_Data)
+# popstep <- stepAIC(poplmfit, direction="both")
+# summary(popstep)
+# stepformula <- formula(terms(popstep))
+# commented out b/c formula saved below:
+
+stepformula <- pop_2010 ~ SS_recip + IRS_returns + rent_201501 + homeprice + 
+  valuechange_5year + com_elecrate + res_elecrate + beds + 
+  gas_stations + fastfood + towers + care_centers + home_daycare + 
+  farmers_markets + walmart + cvs + lowes + whole_foods + starbucks + 
+  beds_avg + gas_stations_avg + fastfood_avg + care_centers_avg + 
+  target_avg + lowes_avg + starbucks_avg + SS_recip_sum + IRS_returns_sum + 
+  rent_sum + homeprice_sum + SS_recip_avg + IRS_returns_avg + 
+  rent_avg + homeprice_avg + beds_sum + gas_stations_sum + 
+  fastfood_sum + towers_sum + care_centers_sum + home_daycare_sum + 
+  farmers_markets_sum + walmart_sum + target_sum + cvs_sum + 
+  home_depot_sum + lowes_sum + whole_foods_sum + basspro_sum + 
+  SS_imputed + IRS_imputed + rent_imputed + valchange_imputed + 
+  latitude + longitude
 
 popfitsimple <- lm(pop_2010 ~ SS_recip + IRS_returns, data=Pop_Model_Data)
 summary(popfitsimple)
 
 # shuffle data
-source("../code/utils/crossval_utils.R")
+source("../utils/crossval_utils.R")
 set.seed(11235)
 Pop_Model_Data_Shuffled <- shuffle(Pop_Model_Data)
 
@@ -51,8 +67,6 @@ mean(rsq_vals_simple)
 boxplot(rsq_vals_simple)
 
 # test stepwise-selected model
-stepformula <- formula(terms(popstep))
-
 k=10
 iters <- seq(1,k,by=1)
 rsq_vals_step <- rep(NA,k)
