f96bdbb92f29d1b6b11778922c52c7a112c84951
DerrickStuckey <derrickstuckey@gmail.com>
2015-08-05 17:16:43 -0400

    pct bachelor's model with log-odds transform

---
+++ code/analysis/edu_analysis_bach_logodds.R
@@ -0,0 +1,49 @@
+## exploratory analysis of education demographics w/ regression analysis
+library(MASS)
+library(rpart)
+library(randomForest)
+source("../utils/census_utils.R")
+source("../utils/crossval_utils.R")
+
+# load predictors
+Practicum_Predictors_Normalized <- read.csv("../../prepared_data/Practicum_Predictors_Normalized.csv", stringsAsFactors=FALSE)
+
+# load targets
+Practicum_Targets <- read.csv("../../prepared_data/Practicum_Targets.csv", stringsAsFactors=FALSE)
+
+# remove data w/o education values
+Practicum_Targets <- Practicum_Targets[!is.na(Practicum_Targets$perc_bachelors),]
+
+# carve out holdout data
+Model_Target_Data <- Practicum_Targets[Practicum_Targets$partition==1,]
+Test_Target_Data <- Practicum_Targets[Practicum_Targets$partition==0,]
+
+# construct model dataset for education
+Edu_Model_Target_Data <- subset(Model_Target_Data, select=c(zipCode,perc_bachelors))
+Edu_Model_Data <- merge(Edu_Model_Target_Data,Practicum_Predictors_Normalized,by="zipCode",
+                        all.x=TRUE,all.y=FALSE)
+
+# shuffle data
+set.seed(11235)
+Edu_Model_Data_Shuffled <- shuffle(Edu_Model_Data)
+
+# transform target variable to log-odds form
+Edu_Model_Data_Shuffled$perc_bachelors_logodds <- sapply(Edu_Model_Data_Shuffled$perc_bachelors,
+                                                                   transform_pct_log_odds)
+summary(Edu_Model_Data_Shuffled$perc_bachelors_logodds)
+
+# try basic linear regression cross-validation performance
+k=5
+iters <- seq(1,k,by=1)
+rsq_vals_bach_log <- rep(NA,k)
+for (iter in iters) {
+  training_cur <- select_training(Edu_Model_Data_Shuffled,k,iter)
+  validation_cur <- select_validation(Edu_Model_Data_Shuffled,k,iter)
+  fit_cur <- lm(perc_bachelors_logodds ~ .-zipCode-state_code, data=training_cur)
+  preds_cur_transformed <- predict(fit_cur,newdata=validation_cur)
+  preds_cur <- sapply(preds_cur_transformed,untransform_pct_log_odds)
+  rsq_cur <- rsq_val(preds_cur,validation_cur$perc_bachelors)
+  rsq_vals_bach_log[iter] <- rsq_cur
+}
+
+mean(rsq_vals_bach_log)
--- code/utils/census_utils.R
+++ code/utils/census_utils.R
@@ -5,3 +5,21 @@ rsq_val <- function(preds, actuals) {
   sse <- sum((actuals - preds)^2)
   return (1 - sse/sst)
 }
+
+## log-odds transform for percentage variable
+transform_pct_log_odds <- function(x) {
+  # constrain x to interval [1,99] to avoid returning INF
+  x <- min(x,99)
+  x <- max(x,1)
+  odds_x <- x / (100 - x)
+  log_odds_x <- log(odds_x)
+  return(log_odds_x)
+}
+
+## reverse log-odds transform for percentage variable
+untransform_pct_log_odds <- function(log_odds_x) {
+  odds_x <- exp(log_odds_x)
+  x <- 100*(odds_x) / (1+odds_x)
+  return(x)
+}
+
