6af5a4f2c3adeb2505931991b3a75e27d56c2484
DerrickStuckey <derrickstuckey@gmail.com>
2015-08-05 14:47:28 -0400

    RF crossval performance measured

--- code/analysis/edu_analysis_bach.R
+++ code/analysis/edu_analysis_bach.R
@@ -1,6 +1,7 @@
 ## exploratory analysis of education demographics w/ regression analysis
 library(MASS)
 library(rpart)
+library(randomForest)
 source("../utils/census_utils.R")
 source("../utils/crossval_utils.R")
 
@@ -11,15 +12,14 @@ Practicum_Predictors_Normalized <- read.csv("../../prepared_data/Practicum_Predi
 Practicum_Targets <- read.csv("../../prepared_data/Practicum_Targets.csv", stringsAsFactors=FALSE)
 
 # remove data w/o education values
-Practicum_Targets <- Practicum_Targets[!is.na(Practicum_Targets$perc_bachelors) & 
-                                         !is.na(Practicum_Targets$perc_graddegree),]
+Practicum_Targets <- Practicum_Targets[!is.na(Practicum_Targets$perc_bachelors),]
 
 # carve out holdout data
 Model_Target_Data <- Practicum_Targets[Practicum_Targets$partition==1,]
 Test_Target_Data <- Practicum_Targets[Practicum_Targets$partition==0,]
 
 # construct model dataset for education
-Edu_Model_Target_Data <- subset(Model_Target_Data, select=c(zipCode,perc_bachelors,perc_graddegree))
+Edu_Model_Target_Data <- subset(Model_Target_Data, select=c(zipCode,perc_bachelors))
 Edu_Model_Data <- merge(Edu_Model_Target_Data,Practicum_Predictors_Normalized,by="zipCode",
                         all.x=TRUE,all.y=FALSE)
 
@@ -31,7 +31,7 @@ Edu_Model_Data_Shuffled <- shuffle(Edu_Model_Data)
 ## Fit stepwise regression for percent bachelor's
 ##
 
-# bachlmfit <- lm(perc_bachelors ~ .-zipCode-perc_graddegree, data=Edu_Model_Data)
+# bachlmfit <- lm(perc_bachelors ~ .-zipCode-state_code, data=Edu_Model_Data)
 # bachstep <- stepAIC(bachlmfit, direction="both")
 # summary(bachstep)
 # stepformula_bach <- formula(terms(bachstep))
@@ -48,7 +48,7 @@ stepformula_bach <- perc_bachelors ~ SS_recip + IRS_returns + rent_201501 + home
   valchange_imputed + latitude + longitude
 
 # and test the model's performance w/ cross-validation
-k=10
+k=5
 iters <- seq(1,k,by=1)
 rsq_vals_bach_step <- rep(NA,k)
 for (iter in iters) {
@@ -66,19 +66,19 @@ boxplot(rsq_vals_bach_step,main="Crossval R-sq Pct Bachelors Stepwise")
 ##
 ## Fit decision tree for percent bachelor's
 ##
-bach_tree_fit <- rpart(perc_bachelors ~ .-zipCode-perc_graddegree, 
+bach_tree_fit <- rpart(perc_bachelors ~ .-zipCode-state_code, 
                        method="anova",
                        data=Edu_Model_Data_Shuffled,)
 bach_tree_preds <- predict(bach_tree_fit,newdata=Edu_Model_Data_Shuffled)
 bach_tree_rsq <- rsq_val(bach_tree_preds,Edu_Model_Data_Shuffled$perc_bachelors)
 
-k=10
+k=5
 iters <- seq(1,k,by=1)
 rsq_vals_bach_tree <- rep(NA,k)
 for (iter in iters) {
   training_cur <- select_training(Edu_Model_Data_Shuffled,k,iter)
   validation_cur <- select_validation(Edu_Model_Data_Shuffled,k,iter)
-  bach_tree_cur <- rpart(perc_bachelors ~ .-zipCode-perc_graddegree, 
+  bach_tree_cur <- rpart(perc_bachelors ~ .-zipCode-state_code, 
                          method="anova",
                          data=training_cur)
   preds_cur <- predict(bach_tree_cur,newdata=validation_cur)
@@ -92,12 +92,34 @@ boxplot(rsq_vals_bach_tree,main="Crossval R-sq Pct Bachelors Tree")
 ##
 ## random forest model for pct bachelors
 ##
-library(randomForest)
-bach_forest_fit <- randomForest(perc_bachelors ~ .-zipCode-perc_graddegree, data=Edu_Model_Data_Shuffled)
+
+# Random Forest cross-validation performance
+k=5
+iters <- seq(1,k,by=1)
+rsq_vals_bach_rf <- rep(NA,k)
+for (iter in iters) {
+  training_cur <- select_training(Edu_Model_Data_Shuffled,k,iter)
+  validation_cur <- select_validation(Edu_Model_Data_Shuffled,k,iter)
+  bach_rf_cur <- randomForest(perc_bachelors ~ .-zipCode-state_code, data=training_cur,
+                              ntree=200,mtry=20,maxnodes=200)
+  preds_cur <- predict(bach_rf_cur,newdata=validation_cur)
+  rsq_cur <- rsq_val(preds_cur,validation_cur$perc_bachelors)
+  rsq_vals_bach_rf[iter] <- rsq_cur
+}
+
+mean(rsq_vals_bach_rf)
+boxplot(rsq_vals_bach_rf,main="Crossval R-sq Pct Bachelors RF")
+# observed rsq: 0.5694
+
+# RF model w/ full training data
+bach_forest_fit <- randomForest(perc_bachelors ~ .-zipCode-state_code, data=Edu_Model_Data_Shuffled,
+                                ntree=200,mtry=20,maxnodes=200)
 print(bach_forest_fit) # view results 
 importance(bach_forest_fit) # importance of each predictor
 #save the model
 save(bach_forest_fit,file="saved_models/bach_deg_rf.rda")
+
+
 
 ##
 ## test on holdout data
