1c88b4e1e74f81a71f3ee2576a0e462e3fb87862
DerrickStuckey <derrickstuckey@gmail.com>
2015-08-06 23:20:55 -0400

    added multi-stage model

--- code/analysis/pop_analysis_trees.R
+++ code/analysis/pop_analysis_trees.R
@@ -1,6 +1,7 @@
 ## exploratory analysis of population w/ regression analysis
 library(MASS)
 library(rpart)
+library(randomForest)
 source("../utils/census_utils.R")
 source("../utils/crossval_utils.R")
 
@@ -76,7 +77,72 @@ boxplot(rsq_vals_pop_rf,main="Crossval R-sq Population RF")
 # construct model w/ same params, using full training data
 pop_rf_full <- randomForest(pop_2010 ~ .-zipCode-state_code, data=Pop_Model_Data_Shuffled,
                            ntree=200,mtry=20,maxnodes=200)
-save(pop_rf_full,file="saved_models/pop_rf.rda")
+# save(pop_rf_full,file="saved_models/pop_rf.rda")
 
 # check results
 print(pop_rf_full)
+
+# order by importance
+term_importance <- data.frame("var"=row.names(importance(pop_rf_full)),
+                              importance(pop_rf_full))
+sorted_importance <- term_importance[order(term_importance$IncNodePurity,
+                                           decreasing=TRUE),]
+sorted_importance[0:5,]
+sorted_importance[0:10,]
+
+## importance results:
+# > sorted_importance[0:10,]
+# var IncNodePurity
+# IRS_returns             IRS_returns  2.005089e+12
+# SS_recip                   SS_recip  1.067140e+12
+# care_centers           care_centers  6.520578e+11
+# cvs                             cvs  1.886471e+11
+# starbucks                 starbucks  1.681358e+11
+# gas_stations           gas_stations  1.102722e+11
+# fastfood                   fastfood  1.085229e+11
+# homeprice_imputed homeprice_imputed  8.271870e+10
+# IRS_returns_avg     IRS_returns_avg  6.615690e+10
+# valchange_imputed valchange_imputed  6.058709e+10
+
+
+
+##
+## combined RF and linear models
+##
+k=5
+iters <- seq(1,k,by=1)
+rsq_vals_pop_comb <- rep(NA,k)
+for (iter in iters) {
+  training_cur <- select_training(Pop_Model_Data_Shuffled,k,iter)
+  validation_cur <- select_validation(Pop_Model_Data_Shuffled,k,iter)
+  pop_simple_cur <- lm(pop_2010 ~ SS_recip + IRS_returns,data=training_cur)
+  pop_rf_cur <- randomForest(pop_2010 ~ .-zipCode-state_code, data=training_cur,
+                             ntree=200,mtry=20,maxnodes=200)
+  
+  #fit combined model
+  combined_training_cur <- data.frame("pop_2010"=training_cur$pop_2010,
+                                      "rf_preds"=pop_rf_cur$predicted,
+                                      "lm_preds"=pop_simple_cur$fitted.values)
+  combined_mod_cur <- lm(pop_2010 ~ ., data=combined_training_cur)
+  
+  #construct df for combined mod validation
+  rf_preds_cur <- predict(pop_rf_cur,newdata=validation_cur)
+  simple_preds_cur <- predict(pop_simple_cur,newdata=validation_cur)
+  combined_validation_cur <- data.frame("pop_2010"=validation_cur$pop_2010,
+                                        "rf_preds"=rf_preds_cur,
+                                        "lm_preds"=simple_preds_cur)
+  print("pop_2010, rf_preds_cur, simple_prds_cur:")
+  length(validation_cur$pop_2010)
+  length(rf_preds_cur)
+  length(simple_preds_cur)
+  
+  combined_preds_cur <- predict(combined_mod_cur,newdata=combined_validation_cur)
+  
+  rsq_cur <- rsq_val(combined_preds_cur,validation_cur$pop_2010)
+  rsq_vals_pop_comb[iter] <- rsq_cur
+}
+
+mean(rsq_vals_pop_comb)
+# observed r-sq: 0.9811835
+
+
